8d0ce3d46b2f23eaa32b4c7607c349fc9039de25
allofgame26 <rizkiarya79@yahoo.com>
2024-10-01 14:57:55 +0700

    Jobsheet 5

---
+++ pertemuan4/bin/pertemuan4.dart
@@ -0,0 +1,40 @@
+import 'package:pertemuan4/pertemuan4.dart' as pertemuan4;
+
+void main(List<String> arguments) {
+  // var list = [1, 2, 3];
+  // assert(list.length == 3);
+  // assert(list[1] == 2);
+  // print(list.length);
+  // print(list[1]);
+
+  // list[1] = 1;
+  // assert(list[1] == 1);
+  // print(list[1]);
+
+  // Praktikum 1
+  
+  // final List<dynamic> list = List.filled(5, null); //Mendeklarasi list final
+
+  // list[1] = 'Rizki Arya Prayoga';
+  // list[2] = '2141762051';
+
+  // print("Panjang List : ${list.length}"); // Panjang dari list
+  // print(list[1]);
+  // print(list[2]);
+
+  // print(list); //Outpunya tergantung dengan list
+
+  // Praktikum 2
+  var halogens = {'fluorine', 'chlorine', 'bromine', 'iodine', 'astatine'};
+  print(halogens);
+
+  var names1 = <String>{};
+  Set<String> names2 = {}; // This works, too.
+  var names3 = {}; // Creates a map, not a set.
+
+  names1.add("Rizki Arya Prayoga");
+  names1.add("2141762051");
+  // print(names1);
+  // print(names2);
+  // print(names3);
+}
